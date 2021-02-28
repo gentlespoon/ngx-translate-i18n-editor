c737c1b5580d54b89d731c3e26cde0c933fa2dc9
Angda Song <angda.song@nortonlifelock.com>
2021-02-27 19:22:56 -0800

    Remove sign in requirements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,30 @@
-import 'reflect-metadata';
-import '../polyfills';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import "reflect-metadata";
+import "../polyfills";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
 // NG Translate
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
-import { ElectronService } from './services/electron.service';
+import { ElectronService } from "./services/electron.service";
 
-import { WebviewDirective } from './directives/webview.directive';
+import { WebviewDirective } from "./directives/webview.directive";
 
-import { AppComponent } from './app.component';
-import { KeylistComponent } from './components/keylist-panel/keylist.component';
-import { LanglistComponent } from './components/langlist-panel/langlist.component';
-import { SessionPanelComponent } from './components/session-panel/session-panel.component';
-import { EditorPanelComponent } from './components/editor-panel/editor-panel.component';
-import { GsapiService } from './services/gsapi.service';
-import { TranslationService } from './services/translation.service';
-import { SessionService } from './services/session.service';
-import { I18nService } from './services/i18n.service';
+import { AppComponent } from "./app.component";
+import { KeylistComponent } from "./components/keylist-panel/keylist.component";
+import { LanglistComponent } from "./components/langlist-panel/langlist.component";
+import { SessionPanelComponent } from "./components/session-panel/session-panel.component";
+import { EditorPanelComponent } from "./components/editor-panel/editor-panel.component";
+import { TranslationService } from "./services/translation.service";
+import { I18nService } from "./services/i18n.service";
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
 }
 
 @NgModule({
@@ -37,7 +34,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     KeylistComponent,
     LanglistComponent,
     SessionPanelComponent,
-    EditorPanelComponent
+    EditorPanelComponent,
   ],
   imports: [
     BrowserModule,
@@ -46,13 +43,12 @@ export function HttpLoaderFactory(http: HttpClient) {
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
-        useFactory: (HttpLoaderFactory),
-        deps: [HttpClient]
-      }
-    })
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [ElectronService, GsapiService, TranslationService, SessionService, I18nService],
-  bootstrap: [AppComponent]
+  providers: [ElectronService, TranslationService, I18nService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -1,138 +1,71 @@
-import { Injectable } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { GsapiService } from './gsapi.service';
-import { SessionService } from './session.service';
+import { Injectable } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class TranslationService {
-
   private DEV = false;
 
   public readonly languages = {
-    'en-TT': 'English (WTF?!)',
-    'en-US': 'English (United States)',
-    'en-GB': 'English (United Kingdom)' ,
-    'zh-CHS': '中文 (简体)',
-    'zh-CHT': '中文 (繁體)',
+    "en-TT": "English (WTF?!)",
+    "en-US": "English (United States)",
+    "en-GB": "English (United Kingdom)",
+    "zh-CHS": "中文 (简体)",
+    "zh-CHT": "中文 (繁體)",
   };
 
-
-  constructor(
-    public translateService: TranslateService,
-    private apiService: GsapiService,
-    private sessionService: SessionService,
-  ) {
-
-    this.translateService.addLangs(['en-TT', 'en-US', 'en-GB', 'zh-CHS', 'zh-CHT']);
-    this.translateService.setDefaultLang('en-TT');
+  constructor(public translateService: TranslateService) {
+    this.translateService.addLangs(["en-US", "en-GB", "zh-CHS", "zh-CHT"]);
+    this.translateService.setDefaultLang("en-TT");
     this.getUserLanguage();
   }
 
   public getUserLanguage() {
-    if (this.sessionService.token) {
-      this.getUserLanguageFromServer();
-    } else {
-      this.getUserLanguageFromBrowser();
-    }
-  }
-
-
-  private getUserLanguageFromServer(): void {
-    // first try to get from cloud
-    this.apiService.get('/20190605.account/language', this.sessionService.token)
-    .subscribe(response => {
-      if (response.success) {
-        this.translateService.use(this._language);
-        this.useLanguage(response.data[0]);
-      } else {
-        console.warn('[Translation.Service] getUserLanguageFromServer(): ' + response.data);
-        this.getUserLanguageFromBrowser();
-      }
-    }, error => {
-      this.getUserLanguageFromBrowser();
-    });
-  }
-
-
-  // fallback for get language
-  private getUserLanguageFromBrowser(): void {
     // if user has visited before and has a cached language, use it
-    var localLanguage = localStorage.getItem('language');
-    console.log('[Translation.Service] getUserLanguageFromBrowser(): cachedLanguage=', localLanguage)
+    var localLanguage = localStorage.getItem("language");
+    console.log(
+      "[Translation.Service] getUserLanguageFromBrowser(): cachedLanguage=",
+      localLanguage
+    );
     if (localLanguage) {
       this.useLanguage(localLanguage);
     } else {
       // otherwise, use browser default language
       var lang = this.translateService.getBrowserCultureLang();
       switch (lang) {
-        case 'en-US':
-        case 'en-GB':
-          lang = 'en-TT';
+        case "zh-TW":
+        case "zh-HK":
+        case "zh-MO":
+          lang = "zh-CHT";
           break;
-        case 'zh-TW':
-        case 'zh-HK':
-        case 'zh-MO':
-          lang = 'zh-CHT';
-          break;
-        case 'zh-CN':
-        case 'zh-SG':
-          lang = 'zh-CHS';
+        case "zh-CN":
+        case "zh-SG":
+          lang = "zh-CHS";
           break;
       }
       this.useLanguage(lang);
     }
   }
 
-
-
-
-  public saveUserLanguage() : void {
-    if (this.sessionService.token) {
-      this.saveUserLanguageToServer();
-    }
-    // always save to browser
-    this.saveUserLanguageToBrowser();
+  public saveUserLanguage(): void {
+    localStorage.setItem("language", this._language);
   }
 
-
-  private saveUserLanguageToServer(): void {
-    this.apiService.put('/20190605.account/language', this.sessionService.token, {language: this._language})
-    .subscribe(response => {
-      if (response.success) {
-      } else {
-        console.error('[Translation.Service] saveUserLanguageToServer(): ', response.data);
-        throw(response.data);
-      }
-    },
-    error => {
-      console.error('[Translation.Service] saveUserLanguageToServer(): ', error);
-    })
-  }
-
-  // fallback for save language
-  private saveUserLanguageToBrowser(): void  {
-    localStorage.setItem('language', this._language);
-  }
-
-
-  private _language: string = 'en-TT';
+  private _language: string = "en-TT";
 
   public get language(): string {
     return this._language;
   }
 
   public useLanguage(lang: string): void {
-    console.log('[Translation.Service] useLanguage(' + lang + ')');
+    console.log("[Translation.Service] useLanguage(" + lang + ")");
     this._language = lang;
     this.saveUserLanguage();
     this.translateService.use(this._language);
   }
 
-
   public instant(lang: string): string {
     return this.translateService.instant(lang);
   }
-
 }
